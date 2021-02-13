0824148549269313a5b3394db539e7a24f3418b1
Leonardo Casagrande <leoc.casagrande@hotmail.com>
2021-02-13 18:38:39 -0300

    Aula 151

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import classes from './Toolbar.css'
+import classes from './Toolbar.css';
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems'
+import MenuButton from '../SideDrawer/MenuButton/MenuButton';
 
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
-        <div>MENU</div>
-        <div>LOGO</div>
-        <nav>
-            ...
+        <MenuButton clicked={props.opened}/>
+        <div className={classes.Logo}>
+            <Logo />
+        </div>
+        <nav className={classes.DesktopOnly}>
+            <NavigationItems />
         </nav>
     </header>
 );
-    
+
 
 export default toolbar;
