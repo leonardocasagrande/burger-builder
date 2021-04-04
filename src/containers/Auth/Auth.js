0a4e44b78d75f08670abace02ab3021d2f0ede3b
Leonardo Casagrande <leoc.casagrande@hotmail.com>
2021-04-04 16:20:52 -0300

    Ended improvements

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -9,6 +9,7 @@ import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler'
 import classes from './Auth.css';
 import * as actions from '../../store/actions/index';
 import Spinner from '../../components/UI/Spinner/Spinner';
+import { checkValidity, updateObject } from '../../shared/utility';
 
 class Auth extends Component {
 
@@ -52,44 +53,20 @@ class Auth extends Component {
         }
     }
 
-    checkValidity(value, rules) {
-        let isValid = true;
-        if (rules.required) {
-            isValid = value.trim() !== '' && isValid;
-        }
-
-        if (rules.minLength) {
-            isValid = value.length >= rules.minLength && isValid;
-        }
-
-        if (rules.maxLength) {
-            isValid = value.length <= rules.minLength && isValid;
-        }
-
-        if (rules.isEmail) {
-            const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-            isValid = pattern.test(value) && isValid;
-        }
-
-        if (rules.isNumeric) {
-            const pattern = /^\d+$/;
-            isValid = pattern.test(value) && isValid;
-        }
-
-        return isValid;
-    }
-
     inputChangedHandler = (event, controlName) => {
-        const updatedControls = {
-            ...this.state.controls,
-            [controlName]: {
-                ...this.state.controls[controlName],
-                value: event.target.value,
-                valid: this.checkValidity(event.target.value,
-                    this.state.controls[controlName].validation),
-                touched: true
-            }
-        }
+        const updatedControls = updateObject(
+            this.state.controls,
+            {
+                [controlName]: updateObject(
+                    this.state.controls[controlName],
+                    {
+                        value: event.target.value,
+                        valid: checkValidity(event.target.value,
+                            this.state.controls[controlName].validation),
+                        touched: true
+                    }
+                )
+            });
         this.setState({ controls: updatedControls })
     }
 
